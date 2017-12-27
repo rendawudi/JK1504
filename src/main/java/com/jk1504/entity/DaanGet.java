c1168855201636e962b45871a4a01c9fa9acb14b
renda <122700138@qq.com>
2017-12-27 21:31:17 +0800

    增加返回已经完成任务的列表 增加返回错误率最高的题目列表

--- src/main/java/com/jk1504/entity/DaanGet.java
+++ src/main/java/com/jk1504/entity/DaanGet.java
@@ -2,7 +2,6 @@ package com.jk1504.entity;
 
 public class DaanGet {
     Integer timuid;//需要前端填充timuid,stuid
-    Integer stuid;
     Boolean rightdaan;//对错
 
     public Integer getTimuid() {
@@ -13,13 +12,6 @@ public class DaanGet {
         this.timuid = timuid;
     }
 
-    public Integer getStuid() {
-        return stuid;
-    }
-
-    public void setStuid(Integer stuid) {
-        this.stuid = stuid;
-    }
 
     public Boolean getRightdaan() {
         return rightdaan;
